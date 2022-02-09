7772423cbad00bebb880fe6b7f2dac40ccc80a80
Aisulu <kalpena11@gmail.ru>
2022-02-09 18:29:43 +0600

    scale

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import sys
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 from show_map import show_map
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
 
 class Map(QWidget):
     def __init__(self):
         super().__init__()
         self.lat = 51.119461
         self.lon = 71.431741
-        self.spn = 0.01
+        self.spn = 0.01          # default
         self.mode = "sat"
-        self.create_map()
+        self.create_map(new=True)
         self.initUI()
 
 
     def initUI(self):
         self.setGeometry(300, 300, 700, 700)
         self.setWindowTitle('Координаты')
-        self.pixmap = QPixmap('map.png')
+
         self.map = QLabel(self)
         self.map.setPixmap(self.pixmap)
         self.map.resize(700, 700)
@@ -27,13 +29,31 @@ class Map(QWidget):
         self.coords.setText("Координаты: None, None")
         self.coords.move(0, 0)
 
-    def update(self):
-        pass
+    def keyPressEvent(self, event):
+        print(1)
+        print(self.spn)
+        if event.key() == Qt.Key_PageUp:
+            self.spn += self.spn / 8
+            print(2)
+            self.create_map()
+        elif event.key() == Qt.Key_PageDown:
+            self.spn -= self.spn / 8
+            self.create_map()
+        if self.spn > 0.05:
+            self.spn = 0.05
+        elif self.spn < 0.001:
+            self.spn = 0.001
 
-    def create_map(self):
+
+
+    def create_map(self, new = False):
         ll_spn = f"ll={self.lon},{self.lat}&spn={self.spn},{self.spn}"
         show_map(ll_spn, map_type=self.mode)
-
+        self.pixmap = QPixmap('map.png')
+        if not new:
+            print(self.spn)
+            self.map.setPixmap(self.pixmap)
+            print(1)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
